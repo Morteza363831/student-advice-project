7e48e4675abd5708005176ebc017e5ac3f079545
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-08 13:09:23 +0430

    project created

---
+++ src/test/java/com/example/studentadviceproject/StudentAdviceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentadviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAdviceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
